c23977d6cd21fddb2f2dd77bc6770eec518b7102
Joe Fabisevich <mergesort@users.noreply.github.com>
2017-03-29 22:32:23 -0400

    Updating DispatchTime+AfterNow for Swift 3.1

--- templates/DispatchTime+AfterNow.swift
+++ templates/DispatchTime+AfterNow.swift
@@ -2,6 +2,11 @@ import Foundation
 
 extension DispatchTime {
 
+    /// This function is shorthand for returning the current `DispatchTime` plus a certain interval,
+    /// useful for calculating a certain time interval after the present time.
+    ///
+    /// - Parameter interval: The `DispatchTimeInterval` to add onto the current `DispatchTime`.
+    /// - Returns: Returns a dispatch time by adding the current `DispatchTime` with the passed in interval.
     static func afterNow(interval: DispatchTimeInterval) -> DispatchTime {
         return DispatchTime.now() + interval
     }
@@ -9,6 +14,11 @@ extension DispatchTime {
 
 extension DispatchQueue {
 
+    /// This function calls `DispatchQueue.asyncAfter` with a certain `DispatchTimeInterval` after now.
+    ///
+    /// - Parameters:
+    ///   - interval: The `DispatchTimeInterval` to add onto the current `DispatchTime`.
+    ///   - work: An block to invoke after the interval has elapsed.
     func asyncAfterNow(interval: DispatchTimeInterval, execute work: @escaping @convention(block) () -> Void) {
         self.asyncAfter(deadline: DispatchTime.afterNow(interval: interval), execute: work)
     }
