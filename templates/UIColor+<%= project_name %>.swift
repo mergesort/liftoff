aceec5c0506b426a301990129076af5ca4f04cb6
Joe Fabisevich <mergesort@users.noreply.github.com>
2017-03-31 20:39:43 -0400

    Adding documentable format for UIColors

--- templates/UIColor+<%= project_name %>.swift
+++ templates/UIColor+<%= project_name %>.swift
@@ -1,13 +1,15 @@
 import UIKit
 
 public extension UIColor {
-
+    
+    /// #222222
     public static var paletteBlack: UIColor {
-        return #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1) // #222222
+        return #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
     }
 
+    /// #FEFEFE
     public static var paletteWhite: UIColor {
-        return #colorLiteral(red: 0.9960784314, green: 0.9960784314, blue: 0.9960784314, alpha: 1) // #FEFEFE
+        return #colorLiteral(red: 0.9960784314, green: 0.9960784314, blue: 0.9960784314, alpha: 1)
     }
 
 }
